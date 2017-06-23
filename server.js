23c5875db03ac4cf06342c2e1f71d4cdd6a68781
asw001 <variableapple@gmail.com>
2017-06-22 21:27:29 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,79 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
 const app = express();
+// Mongoose internally uses a promise-like object,
+// but its better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
 
-const blogRouter = require('./blogRouter');
+// config.js is where we control constants for entire
+// app like PORT and DATABASE_URL
+const {PORT, DATABASE_URL} = require('./config');
+const {BlogPosts} = require('./models');
+
+
+//const {blogapp} = require('./blogapp');
 
 app.use(morgan('common'));
-app.use('/blog-posts', blogRouter);
+//app.use('/blog-posts', blogapp);
 
 //app.listen(process.env.PORT || 8080, () => {
 //  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 //});
+
+app.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+app.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
+app.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog list item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+app.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).json(updatedItem);
+})
 
 let server;
 
