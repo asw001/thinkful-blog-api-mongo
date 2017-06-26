46327ed46cb867ccbd009d1f1d4d84a06475f5e7
asw001 <variableapple@gmail.com>
2017-06-25 21:48:33 -0500

    working

--- server.js
+++ server.js
@@ -1,117 +1,158 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 const app = express();
-// Mongoose internally uses a promise-like object,
-// but its better to make Mongoose use built in es6 promises
+
+app.use(morgan('common'));
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-// config.js is where we control constants for entire
-// app like PORT and DATABASE_URL
 const {PORT, DATABASE_URL} = require('./config');
 const {BlogPosts} = require('./models');
 
 
-//const {blogapp} = require('./blogapp');
-
-app.use(morgan('common'));
-//app.use('/blog-posts', blogapp);
-
-//app.listen(process.env.PORT || 8080, () => {
-//  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-//});
-
-app.get('/', (req, res) => {
-  res.json(BlogPosts.get());
+app.get('/posts/', (req, res) => {
+    BlogPosts.find()
+        .exec()
+        .then(posts => {
+            res.json(posts.map(post => post.apiRepr()));
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: 'ERROR: there was a problem retrieving records'
+            });
+        });
 });
 
-app.post('/', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
+app.get('/posts/:id', (req, res) => {
+    BlogPost
+        .findById(req.params.id)
+        .exec()
+        .then(post => res.json(post.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: 'ERROR: there was a problem retrieving the record'
+            });
+        });
+});
+
+app.post('/posts/', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+   }
+    BlogPost.create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        })
+        .then(blogPost => res.status(201).json(blogPost.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: 'ERROR: there was a problem submitting your blog post'
+            })
+        });
+});
+
+app.delete('posts/:id', (req, res) => {
+    BlogPosts.findByIdAndRemove(req.params.id)
+        .exec()
+        .then(() => {
+            res.status(204).json({
+                message: 'INFO: post successfully deleted'
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: 'ERROR: there was a problem deleting the blog post'
+            });
+        });
+});
+
+app.put('posts/:id', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
     }
-  }
-  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-  res.status(201).json(item);
-});
-
-app.delete('/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog list item \`${req.params.ID}\``);
-  res.status(204).end();
-});
-
-
-app.put('/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
+    if (req.params.id !== req.body.id) {
+        const message = (
+            `Request path id (${req.params.id}) and request body id `
+            `(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).send(message);
     }
-  }
-  if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating blog post \`${req.params.id}\``);
-  const updatedItem = BlogPosts.update({
-    id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    publishDate: req.body.publishDate
-  });
-  res.status(204).json(updatedItem);
-})
+    console.log(`Updating blog post \`${req.params.id}\``);
+
+    const updated = {};
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
+    BlogPost.findByIdAndUpdate(req.params.id, {
+            $set: updated
+        }, {
+            new: true
+        })
+        .exec()
+        .then(updatedPost => res.status(204).json(updatedPost.apiRepr()))
+    .catch(err => res.status(500).json({
+        message: 'ERROR: there was a problem updating the post'
+    }));
+});
 
 let server;
 
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                    console.log(`Your app is listening on port ${port}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
     });
-  });
 }
 
-// like `runServer`, this function also needs to return a promise.
-// `server.close` does not return a promise on its own, so we manually
-// create one.
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
     });
-  });
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+    runServer().catch(err => console.error(err));
 };
 
-module.exports = {app, runServer, closeServer};
+module.exports = {app,runServer,closeServer};
