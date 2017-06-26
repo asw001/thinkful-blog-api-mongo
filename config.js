dba6a86603b88ccc90f59e050fc4398b93ff1b0a
asw001 <variableapple@gmail.com>
2017-06-26 08:04:58 -0500

    more changes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+                      'mongodb://svacct1:M0ngoMeanTime@ds135552.mlab.com:35552/blogs';
 exports.PORT = process.env.PORT || 8080;
